8078291a3242152a89d974442a06b220653cd3ab
hi <sharmryl000@k12.prosper-isd.net>
2026-05-19 13:20:42 -0500

    Initialize __init__.py with layer imports

---
+++ src/megatron/layers/__init__.py
@@ -0,0 +1,6 @@
+
+
+from .parallel_attention import ParallelLlamaAttention
+from .parallel_decoder import ParallelLlamaDecoderLayer, ParallelLlamaDecoderLayerRmPad
+from .parallel_mlp import ParallelLlamaMLP
+from .parallel_rmsnorm import ParallelLlamaRMSNorm
