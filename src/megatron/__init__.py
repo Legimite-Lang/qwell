0dca369f56dfa9a688db1e9c92858d17f4099217
hi <sharmryl000@k12.prosper-isd.net>
2026-05-19 13:30:43 -0500

    Initialize megatron module and import models

---
+++ src/megatron/__init__.py
@@ -0,0 +1,12 @@
+
+
+from .modeling_llama_megatron import (
+    # original model with megatron
+    ParallelLlamaModel,
+    ParallelLlamaForCausalLM,
+    # rmpad with megatron
+    ParallelLlamaForCausalLMRmPad,
+    ParallelLlamaForValueRmPad,
+    # rmpad with megatron and pipeline parallelism
+    ParallelLlamaForCausalLMRmPadPP,
+    ParallelLlamaForValueRmPadPP)
